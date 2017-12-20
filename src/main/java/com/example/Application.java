67b145f9b0ad57213102d39b743a45bf81ada99e
Andrew De Ponte <cyphactor@gmail.com>
2017-12-20 10:53:43 -0800

    Formatting for consistency

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -8,7 +8,7 @@ import org.springframework.http.*;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Example.class, args);
-    }
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Example.class, args);
+  }
 }
