d2a709a901d67abd08c8f76752d5a952272accf8
Andrew De Ponte <cyphactor@gmail.com>
2017-12-20 10:45:44 -0800

    Eliminate tabs and correct formatting
    This is strictly a commit changing the format of the text, it should not
    and is not changing any of the logic or the code itself.

--- src/test/java/com/example/HttpRequestTest.java
+++ src/test/java/com/example/HttpRequestTest.java
@@ -17,65 +17,65 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
 
-		@LocalServerPort
-		private int port;
+  @LocalServerPort
+  private int port;
 
-		@Autowired
-		private TestRestTemplate restTemplate;
+  @Autowired
+  private TestRestTemplate restTemplate;
 
-		@Test
-		public void testRoot() throws Exception {
-			HttpHeaders headers = new HttpHeaders();
-			headers.set("Accept", "application/vnd.example.v2+json");
-			headers.set("Content-Type", "application/vnd.example.v2+json");
-            String bodyTesting = "{ \"test\": \"phil\" }";
-            HttpEntity<String> entity = new HttpEntity<String>(bodyTesting, headers);
+  @Test
+  public void testRoot() throws Exception {
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Accept", "application/vnd.example.v2+json");
+    headers.set("Content-Type", "application/vnd.example.v2+json");
+    String bodyTesting = "{ \"test\": \"phil\" }";
+    HttpEntity<String> entity = new HttpEntity<String>(bodyTesting, headers);
 
-            ResponseEntity<String> exchange = restTemplate.exchange("http://localhost:" + port, HttpMethod.POST, entity, String.class);
+    ResponseEntity<String> exchange = restTemplate.exchange("http://localhost:" + port, HttpMethod.POST, entity, String.class);
 
-            assertEquals(HttpStatus.OK, exchange.getStatusCode());
-            assertEquals("application/vnd.example.v8+json", exchange.getHeaders().get("Content-Type").get(0));
-            assertEquals("{ \"accept\": \"application/vnd.example.v2+json\", \"content-type\": \"application/vnd.example.v2+json\", \"body\": \"{ \"test\": \"phil\" }\" }", exchange.getBody());
-        }
+    assertEquals(HttpStatus.OK, exchange.getStatusCode());
+    assertEquals("application/vnd.example.v8+json", exchange.getHeaders().get("Content-Type").get(0));
+    assertEquals("{ \"accept\": \"application/vnd.example.v2+json\", \"content-type\": \"application/vnd.example.v2+json\", \"body\": \"{ \"test\": \"phil\" }\" }", exchange.getBody());
+  }
 
-        @Test
-        public void testSuccessfullyCreateHelloMessageJson() throws Exception {
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Accept", "application/json");
-            headers.set("Content-Type", "application/json");
-            String bodyTesting = "{ \"first_name\": \"Bob\", \"last_name\": \"Villa\" }";
-            HttpEntity<String> entity = new HttpEntity<String>(bodyTesting, headers);
+  @Test
+  public void testSuccessfullyCreateHelloMessageJson() throws Exception {
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Accept", "application/json");
+    headers.set("Content-Type", "application/json");
+    String bodyTesting = "{ \"first_name\": \"Bob\", \"last_name\": \"Villa\" }";
+    HttpEntity<String> entity = new HttpEntity<String>(bodyTesting, headers);
 
-            ResponseEntity<String> exchange = restTemplate.exchange("http://localhost:" + port + "/hello", HttpMethod.POST, entity, String.class);
+    ResponseEntity<String> exchange = restTemplate.exchange("http://localhost:" + port + "/hello", HttpMethod.POST, entity, String.class);
 
-            assertEquals(HttpStatus.OK, exchange.getStatusCode());
-            assertEquals("application/json;charset=UTF-8", exchange.getHeaders().get("Content-Type").get(0));
-            assertEquals("{\"message\":\"Hello, Bob Villa\"}", exchange.getBody());
-        }
+    assertEquals(HttpStatus.OK, exchange.getStatusCode());
+    assertEquals("application/json;charset=UTF-8", exchange.getHeaders().get("Content-Type").get(0));
+    assertEquals("{\"message\":\"Hello, Bob Villa\"}", exchange.getBody());
+  }
 
-        @Test
-        public void testNotAcceptableCreateMessage() throws Exception {
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Accept", "application/vnd.example.v21+json");
-            headers.set("Content-Type", "application/vnd.example.v1+json");
-            String bodyTesting = "{ \"first_name\": \"Bob\", \"last_name\": \"Villa\" }";
-            HttpEntity<String> entity = new HttpEntity<String>(bodyTesting, headers);
+  @Test
+  public void testNotAcceptableCreateMessage() throws Exception {
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Accept", "application/vnd.example.v21+json");
+    headers.set("Content-Type", "application/vnd.example.v1+json");
+    String bodyTesting = "{ \"first_name\": \"Bob\", \"last_name\": \"Villa\" }";
+    HttpEntity<String> entity = new HttpEntity<String>(bodyTesting, headers);
 
-            ResponseEntity<String> exchange = restTemplate.exchange("http://localhost:" + port + "/hello", HttpMethod.POST, entity, String.class);
+    ResponseEntity<String> exchange = restTemplate.exchange("http://localhost:" + port + "/hello", HttpMethod.POST, entity, String.class);
 
-            assertEquals(HttpStatus.NOT_ACCEPTABLE, exchange.getStatusCode());
-        }
+    assertEquals(HttpStatus.NOT_ACCEPTABLE, exchange.getStatusCode());
+  }
 
-        @Test
-        public void testUnsupportedMediaTypeCreateMessage() throws Exception {
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Accept", "application/vnd.example.v1+json");
-            headers.set("Content-Type", "application/vnd.example.v21+json");
-            String bodyTesting = "{ \"first_name\": \"Bob\", \"last_name\": \"Villa\" }";
-            HttpEntity<String> entity = new HttpEntity<String>(bodyTesting, headers);
+  @Test
+  public void testUnsupportedMediaTypeCreateMessage() throws Exception {
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Accept", "application/vnd.example.v1+json");
+    headers.set("Content-Type", "application/vnd.example.v21+json");
+    String bodyTesting = "{ \"first_name\": \"Bob\", \"last_name\": \"Villa\" }";
+    HttpEntity<String> entity = new HttpEntity<String>(bodyTesting, headers);
 
-            ResponseEntity<String> exchange = restTemplate.exchange("http://localhost:" + port + "/hello", HttpMethod.POST, entity, String.class);
+    ResponseEntity<String> exchange = restTemplate.exchange("http://localhost:" + port + "/hello", HttpMethod.POST, entity, String.class);
 
-            assertEquals(HttpStatus.UNSUPPORTED_MEDIA_TYPE, exchange.getStatusCode());
-        }
+    assertEquals(HttpStatus.UNSUPPORTED_MEDIA_TYPE, exchange.getStatusCode());
+  }
 }
